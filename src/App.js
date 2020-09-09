09d9b35ab14709344bf1c8b5da0bc3adefa22cb3
cleankode <zelalemw@live.com>
2020-09-08 20:16:34 -0700

    clean up navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Header from './coomponents/Header';
+import Header from "./coomponents/Header";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/checkout">
+          <Header />
           <h1>Checkout</h1>
         </Route>
         <Route path="/login">
@@ -22,16 +23,4 @@ function App() {
   );
 }
 
-        {
-          /* we NEED React-Router-Dom */
-        }
-        {
-          /* localhost.com/ */
-        }
-        {
-          /* localhost.com/checkout */
-        }
-        {
-          /* localhost.com/login */
-        }
 export default App;
--- src/coomponents/Header.jsx
+++ src/coomponents/Header.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import SearchIcon from '@material-ui/icons/Search';
-
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingCart";
 import "./Header.css";
 
 function Header() {
   return (
     <nav className="header">
-      {/* logo on the left */}
       <Link to="/">
         <img
           className="header__logo"
@@ -15,15 +14,36 @@ function Header() {
           alt="Amazon logo"
         />
       </Link>
-      {/* search box */}
       <div className="header__search">
         <input type="text" className="header__searchInput" />
         <SearchIcon className="header__searchIcon" />
       </div>
-      {/* 3 links  */}
-      
-
-      {/* basket icon with a number */}
+      <div className="header__nav">
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello Z</span>
+            <span className="header__optionLineTwo">Sign In</span>
+          </div>
+        </Link>
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
+        </Link>
+        <Link>
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__basketCount">0</span>
+          </div>
+        </Link>
+      </div>
     </nav>
   );
 }
