43ca0a79528d7f97f80d7842319ef4ba716dd122
cleankode <zelalemw@live.com>
2020-09-08 21:43:37 -0700

    add product component

--- src/coomponents/Home.jsx
+++ src/coomponents/Home.jsx
@@ -1,18 +1,69 @@
-import React from 'react'
-import './Home.css';
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
 
 function Home() {
-    return (
-      <div className="home">
-        {/* <img src="https://images-eu.ssl-images-amazon.com/images/G/02//> */}
-        {/* https://images-na.ssl-images-amazon.com/images/G/01/IMDbTV/gateway/theoutpost/GW_DHERO_TheOutpost_3000x1200_EN-US_5538_v12x._CB407273330_.jpg */}
-        {/* https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2020/TV/THBY_S2_02111_GWBleedingHero_3000x1200_PRE_Final_en-US_PVD5224._CB410800060_.jpg */}
-        <img className ="home__image"
-          src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/MTc0Y2M5Y2Mt/MTc0Y2M5Y2Mt-ZTMzMTk5Yjct-w3000._CB406837549_.jpg"
-          alt=""
+  return (
+    <div className="home">
+      {/* <img src="https://images-eu.ssl-images-amazon.com/images/G/02//> */}
+      {/* https://images-na.ssl-images-amazon.com/images/G/01/IMDbTV/gateway/theoutpost/GW_DHERO_TheOutpost_3000x1200_EN-US_5538_v12x._CB407273330_.jpg */}
+      {/* https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2020/TV/THBY_S2_02111_GWBleedingHero_3000x1200_PRE_Final_en-US_PVD5224._CB410800060_.jpg */}
+
+      <img
+        className="home__image"
+        src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/MTc0Y2M5Y2Mt/MTc0Y2M5Y2Mt-ZTMzMTk5Yjct-w3000._CB406837549_.jpg"
+        alt=""
+      />
+      <div className="home__row">
+        <Product
+          id="1234"
+          title="The Lean Startup: How Constant Innovation Creates"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/M2RjMzI2ZGUt/M2RjMzI2ZGUt-NjkwMjZhZDEt-w758._SY608_CB406624432_.jpg"
+        />
+        <Product
+          id="1234"
+          title="The Lean Startup: How Constant Innovation Creates"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/M2RjMzI2ZGUt/M2RjMzI2ZGUt-NjkwMjZhZDEt-w758._SY608_CB406624432_.jpg"
         />
       </div>
-    );
+      <div className="home__row">
+        <Product
+          id="1234"
+          title="The Lean Startup: How Constant Innovation Creates"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/M2RjMzI2ZGUt/M2RjMzI2ZGUt-NjkwMjZhZDEt-w758._SY608_CB406624432_.jpg"
+        />
+        <Product
+          id="1234"
+          title="The Lean Startup: How Constant Innovation Creates"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/M2RjMzI2ZGUt/M2RjMzI2ZGUt-NjkwMjZhZDEt-w758._SY608_CB406624432_.jpg"
+        />
+        <Product
+          id="1234"
+          title="The Lean Startup: How Constant Innovation Creates"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/M2RjMzI2ZGUt/M2RjMzI2ZGUt-NjkwMjZhZDEt-w758._SY608_CB406624432_.jpg"
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id="1234"
+          title="The Lean Startup: How Constant Innovation Creates"
+          price={11.96}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/M2RjMzI2ZGUt/M2RjMzI2ZGUt-NjkwMjZhZDEt-w758._SY608_CB406624432_.jpg"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
