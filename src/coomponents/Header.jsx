7f549d7ddafc8fbed4ad47b06adfb288c73a1f0f
cleankode <zelalemw@live.com>
2020-09-08 22:42:16 -0700

    add stateProvider

--- src/coomponents/Header.jsx
+++ src/coomponents/Header.jsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingCart";
 import "./Header.css";
+import {useStateValue} from '../StateProvider';
 
 function Header() {
+  const [{basket}]=useStateValue();
   return (
     <nav className="header">
       <Link to="/">
@@ -40,7 +42,7 @@ function Header() {
         <Link>
           <div className="header__optionBasket">
             <ShoppingBasketIcon />
-            <span className="header__basketCount">0</span>
+            <span className="header__basketCount">{basket.length}</span>
           </div>
         </Link>
       </div>
---
+++ src/reducer.jsx
@@ -0,0 +1,18 @@
+export const initialState = {
+  basket: ['stuff','anothe'],
+};
+
+function reducer(state, action){
+    switch (action.type){
+        case 'ADD_TO_BASKET':
+            // logic to add to basket
+            break;
+        case 'REMOVE_FROM_BASKET':
+            // logic to remove from basket
+            break;
+        default:
+            return state;
+    }
+}
+
+export default reducer;
